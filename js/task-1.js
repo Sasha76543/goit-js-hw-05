32b01f67935c9117dcc71b25f975ee0fcfebd7e5
Sasha <129985153+Sasha76543@users.noreply.github.com>
2024-06-14 18:03:52 +0300

    goit-js-hw-05

--- js/task-1.js
+++ js/task-1.js
@@ -1,25 +1,25 @@
 function Account({ login, email }) {
-    this.login = login;
-    this.email = email;
-  }
-  
-  Account.prototype.getInfo = function() {
-    console.log(`Login: ${this.login}, Email: ${this.email}`);
-  }
-  console.log(Account.prototype.getInfo); // function
+  this.login = login;
+  this.email = email;
+}
 
-  const mango = new Account({
-   login: 'Mangozedog',
-   email: 'mango@dog.woof',
-  });
-  
-  mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
-  
-  const poly = new Account({
-   login: 'Poly',
-   email: 'poly@mail.com',
-  });
-  
-  poly.getInfo(); // Login: Poly, Email: poly@mail.com  
+Account.prototype.getInfo = function () {
+  console.log(`Login: ${this.login}, Email: ${this.email}`);
+};
+console.log(Account.prototype.getInfo); // function
 
-  <script type="module"></script>
+const mango = new Account({
+  login: "Mangozedog",
+  email: "mango@dog.woof",
+});
+
+mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+
+const poly = new Account({
+  login: "Poly",
+  email: "poly@mail.com",
+});
+
+poly.getInfo(); // Login: Poly, Email: poly@mail.com
+
+<script type="module"></script>;
